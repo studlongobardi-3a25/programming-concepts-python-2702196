9c4c7db6ebea00d00699c3b7578af231213f3a72
studlongobardi-3a25 <stud.longobardi@gdromagnosi.edu.it>
2026-04-09 09:46:57 +0000

    modificaes1

--- es1.py
+++ es1.py
@@ -3,3 +3,11 @@ def make_omelette():
   print('pouring the mixture into the frying pan')
   print('cooking the first side')
   print('flipping it')
+  print('cooking the other side\n')
+  omelette = 'a tasty omelette'
+  return omelette
+
+barron_breakfast= make_omelette()
+olivia_breakfast= make_omelette()
+print(f'barron is having {barron_breakfast}\n')
+print(f'olivia is having {olivia_breakfast}\n')
